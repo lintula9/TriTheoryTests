5b88d5fed1c049598d6178c4576977c3a7005675
lintula9 <123648372+lintula9@users.noreply.github.com>
2025-01-13 09:44:55 +0200

    Continuing on making numerical optimizers for the indistinguishable VAR(1).

--- Supplementary Appendix R -script closest indistinguishable VAR(1) function.R
+++ Supplementary Appendix R -script closest indistinguishable VAR(1) function.R
@@ -2,7 +2,7 @@
 # Running (sourcing) the whole script provides the function for the computation.
 
 # Dependencies:
-dependencies <- c("torch", "Matrix")
+dependencies <- c("torch", "Matrix", "numDeriv")
 for( i in dependencies ) {
   
   if( !require(i, character.only = T) ) install.packages( i )
@@ -55,17 +55,72 @@ civ_find <- function( A, Z, n.iter = 1000L ) {
     }
 
 
+
+
+
+# Alternative, simpler.
+
+civ_find2 <- function(A, Z, n.iter = 500) {
+  
+  K <- ncol(A)
+  # Initial values are taken based on A.
+  Lambda <- eigen(A)$vectors[,1]
+  psi    <- eigen(A)$values[1]
+  
+  loss_function <- function(pars, A = A, Z = Z) {
+    
+    P_Lambda = pars[1:K] %*% t(pars[1:K]) * (sum(pars[1:K]^2))^-1
+    M = A %*% P_Lambda - pars[K+1] * P_Lambda + Z - (1-pars[K+1]^2) * pars[1:K] %*% t(pars[1:K])
+    
+    return( sum(M^2) )
+  }
+  
+  gradient_function <- function(pars, A = A, Z = Z) {
+    numDeriv::grad(func = loss_function, x = pars, A = A, Z = Z)
+  }
+  
+  params = c(Lambda,psi)
+  names(params) <- c(paste0("Lambda", 1:K), "psi")
+  optim_result <- optim(par = params, 
+        fn = loss_function, 
+        gr = gradient_function, 
+        method = "BFGS",
+        A = A, Z = Z) 
+  
+  # Extract optimized parameters
+  optimized_params <- optim_result$par
+  Lambda_opt <- optimized_params[1:K]
+  psi_opt <- optimized_params[K + 1]
+  
+  # Compute A_result and Z_result based on optimized values
+  A_result <- psi_opt * Lambda_opt %*% t(Lambda_opt) * (sum(Lambda_opt^2))^-1
+  Z_result <- (1 - psi_opt^2) * Lambda_opt %*% t(Lambda_opt)
+  
+  # Return results
+  return(list(
+    "Loadings" = Lambda_opt, 
+    "psi" = psi_opt,
+    "A_result" = A_result,
+    "Z_result" = Z_result,
+    "Optim_Result" = optim_result
+  ))
+}
+
+
+
 # Not run: Test
 if(F) {
   
   A = matrix(c(0.5,0.2,0.3,
                0.1,0.1,0.1,
                0.2,0.3,0.2), ncol = 3)
-  Z = matrix(c(0.2,0.1,0.0,
-               0.1,0.2,0.0,
+  Z = matrix(c(1.0,0.6,0.0,
+               0.6,1.0,0.0,
                0.0,0.0,0.2), ncol = 3)
   
-  civ_find(A,Z)
+  civ_find2(A, Z)
+  
+  civ_find(A, Z)
   
   
 }
