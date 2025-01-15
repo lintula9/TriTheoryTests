2dbd15b5660a8ab68e34813b4e62a73947916296
Sakari Lintula <Localadmin_lintusak@LH4-345-32304.ad.helsinki.fi>
2025-01-15 17:57:36 +0200

    mproving on estimation. Now using covariance.
    Next step is to define the RMSEA properly and give some numbers.
    Then we are done.

--- Supplementary Appendix R -script closest indistinguishable VAR(1) function.R
+++ Supplementary Appendix R -script closest indistinguishable VAR(1) function.R
@@ -7,6 +7,22 @@ for( i in dependencies ) {
   
   if( !require(i, character.only = T) ) install.packages( i )
   library(i, character.only = T)
+}
+# Helper:
+var_ccov <- function(A,Z,Delta) {
+  
+  return( (A %^% Delta) %*% matrix(Matrix::solve(diag(1,ncol=ncol(A)^2,nrow=nrow(A)^2) - Matrix::kronecker(A,A)) %*% 
+                                     fastmatrix::vec(Z), ncol = ncol(A), nrow = nrow(A)))
+}
+
+# Covariance squared difference
+se_stat <- function(A,Z,A_,Z_,T_=10,nodiag = F) {
+  selector = matrix(T,ncol(A),nrow(A))
+  if(nodiag == T) selector <- lower.tri(A) + upper.tri(A)
+  se = sum(sapply(  1:T_, 
+    function(k) sum( ( var_ccov(A,Z,Delta=k)[selector] - var_ccov(A_,Z_,Delta=k)[selector] )^2 ))) +
+      sum( ( tril(var_ccov(A,Z,Delta=0) - var_ccov(A_,Z_,Delta=0) )[selector ] )^2 )
+  return(se)
   }
 
 # Option 1, based on Torch (does not work?)
@@ -69,10 +85,6 @@ civ_find <- function( A, Z, n.iter = 1000L, tol = 1e-6 ) {
          )
   
     }
-
-
-
-
 
 # Option 2, simpler, seems faster and more reliable.
 
@@ -153,4 +165,140 @@ if(F) {
   
   }
 
-# Evaluate the amount of misfit somehow...
+# Option 3, minimize the discrepancy in cross-covariance.
+#Redunandt, see below.
+
+civ_find3 <- function(A, Z, n.iter = 2000, tol = 1e-6, W = NULL, T_ = 10) {
+  K <- ncol(A)
+  # Initial values are taken based on A.
+  Lambda <- Re(eigen(A)$vectors[,1])
+  psi    <- Re(eigen(A)$values[1])
+  
+  loss_function <- function(pars, A = A, Z = Z) {
+    # Loss based on cross-covariance.
+    
+    Z_ =  (1-pars[K+1]^2) * (pars[1:K] %*% t(pars[1:K])) 
+    A_ = pars[K+1]*pars[1:K] %*% t(pars[1:K]) * sum(pars[1:K]^2)^-1
+    
+    M = sum(sapply(  1:T_, 
+                 function(k) sum( ( var_ccov(A,Z,Delta=k) - var_ccov(A_,Z_,Delta=k) )^2 ))) +
+      sum( (fastmatrix::vech( var_ccov(A,Z,Delta=0) - var_ccov(A_,Z_,Delta=0) ))^2 )
+    return( M )
+  }
+  
+  gradient_function <- function(pars, A = A, Z = Z) {
+    numDeriv::grad(func = loss_function, x = pars, A = A, Z = Z)
+  }
+  
+  params = c(Lambda,psi)
+  names(params) <- c(paste0("Lambda", 1:K), "psi")
+  optim_result <- optim(par = params, 
+                        fn = loss_function, 
+                        gr = gradient_function, 
+                        method = "BFGS",
+                        control = list(abstol = tol, maxit = n.iter),
+                        A = A, Z = Z) 
+  
+  optimized_params <- optim_result$par
+  Lambda_opt <- optimized_params[1:K]
+  psi_opt <- optimized_params[K + 1]
+  
+  C_result <- psi_opt * Lambda_opt %*% t(Lambda_opt) * (sum(Lambda_opt^2))^-1 
+  B_result <- (A - C_result) %*% (diag(1,ncol = ncol(A),nrow = nrow(A)) - 
+                                    Lambda_opt %*% t(Lambda_opt) * sum(Lambda_opt^2)^-1)
+  A_result <- C_result + B_result
+  Z_result <- (1 - psi_opt^2) * Lambda_opt %*% t(Lambda_opt)
+  
+  gc(verbose = F)
+  
+  # Return results
+  return(list(
+    "Loadings" = Lambda_opt, 
+    "psi" = psi_opt,
+    "A_result" = A_result,
+    "B" = B_result,
+    "C" = C_result,
+    "Z_result" = Z_result,
+    "Optim_Result" = optim_result
+  ))
+}
+
+# Option 4, minimize the discrepancy in cross-covariance disregarding diagonal cross-covariance.
+# I.e., we can assume there being idiosyncratic serial correlatedness to the symptoms, not captured by the CF.
+# Thus the diagonal are always a perfect match - as long as the predicted diagonal cross-covariance is less than what the
+# original VAR predicts.
+
+civ_find4 <- function(A, Z, n.iter = 2000, tol = 1e-6, W = NULL, T_ = 10, nodiag = T) {
+  K <- ncol(A)
+  # Initial values are taken based on A.
+  Lambda <- Re(eigen(A)$vectors[,1])
+  psi    <- Re(eigen(A)$values[1])
+  
+  loss_function <- function(pars, A = A, Z = Z) {
+    # Loss based on cross-covariance. Omitting the diagonals of cross-covariances (if the discrepancy is positive).
+    
+    Z_ =  (1-pars[K+1]^2) * (pars[1:K] %*% t(pars[1:K])) 
+    A_ = pars[K+1]*pars[1:K] %*% t(pars[1:K]) * sum(pars[1:K]^2)^-1
+    
+    # Sum the non-diagonal entries
+    nodiag <- lower.tri(A) + upper.tri(A)
+    M = se_stat(A = A,Z = Z,A_ = A_,Z_ = Z_,T_ = T_, nodiag = nodiag)
+    return( M )
+  }
+
+  gradient_function <- function(pars, A = A, Z = Z) {
+    numDeriv::grad(func = loss_function, x = pars, A = A, Z = Z)
+  }
+  
+  params = c(Lambda,psi)
+  names(params) <- c(paste0("Lambda", 1:K), "psi")
+  optim_result <- optim(par = params, 
+                        fn = loss_function, 
+                        gr = gradient_function, 
+                        method = "BFGS",
+                        control = list(abstol = tol, maxit = n.iter),
+                        A = A, Z = Z) 
+  
+  optimized_params <- optim_result$par
+  Lambda_opt <- optimized_params[1:K]
+  psi_opt <- optimized_params[K + 1]
+  
+  C_result <- psi_opt * Lambda_opt %*% t(Lambda_opt) * (sum(Lambda_opt^2))^-1 
+  B_result <- (A - C_result) %*% (diag(1,ncol = ncol(A),nrow = nrow(A)) - 
+                                    Lambda_opt %*% t(Lambda_opt) * sum(Lambda_opt^2)^-1)
+  A_result <- C_result + B_result
+  Z_result <- (1 - psi_opt^2) * Lambda_opt %*% t(Lambda_opt)
+  
+  gc(verbose = F)
+  
+  # Return results
+  return(list(
+    "Loadings" = Lambda_opt, 
+    "psi" = psi_opt,
+    "A_result" = A_result,
+    "B" = B_result,
+    "C" = C_result,
+    "Z_result" = Z_result,
+    "Optim_Result" = optim_result
+  ))
+}
+
+if(F){
+  
+  civ_find3(A, Z) # Result is likely to be pretty close to civ_find2.
+  testres_temp <- civ_find4(A, Z) # Result is likely very different.
+  
+  se_stat(A,Z,testres_temp$A_result,testres_temp$Z_result,nodiag = T)
+  
+}
+
+
+
+
+
+
+
+
+
+
+
--- Supplementary Appendix R -script empiric example.R
+++ Supplementary Appendix R -script empiric example.R
@@ -139,50 +139,59 @@ for( i in 1:nrow(residual_sd_draws)) {
   
 }
 
-# Obtain the closest indistinguishable model, and inference based on the draws from the posterior. -----
-
-# Cross-covariance function
-var_ccov <- function(A,Z,Delta) {
-    
-  return( (A %^% Delta) %*% matrix(Matrix::solve(diag(1,ncol=ncol(A)^2,nrow=nrow(A)^2) - Matrix::kronecker(A,A)) %*% 
-    fastmatrix::vec(Z), ncol = ncol(A), nrow = nrow(A)))
-  }
+## Inference -----
 
 pb <- txtProgressBar(min = 0, max = nrow(draws), style = 3)  # Progress bar
 mad_draws <- c()
 mse_draws <- c()
 rmse_draws <- c()
 rmsea_stat <- c()
-N_ <- nrow(Data5b %>%
-             na.omit())
-for( i in seq(1,N_,by = 10)) {
+for( i in seq(1,nrow(draws),by = 10)) {
   setTxtProgressBar(pb, i)
   #Perform computations
   A <- A_array[ ,, i]
   Z <- Z_array[ ,, i]
-  results <- civ_find2(A, Z)
+  results <- civ_find3(A, Z)
   A_ <- results$A_result
   Z_ <- results$Z_result
   # Compute some summaries of the differences
   mad_draws[ i ] <- mean(abs(c(c(A_ - A),c(Z_ - Z))))
   mse_draws[i] <- mean((c(A_ - A)^2 + c(Z_ - Z)^2))
   rmse_draws[i] <- sqrt(mean((c(A_ - A)^2 + c(Z_ - Z)^2)))
-  # Compute a statistic for RMSE(A), up to arbitrary T = 5.
-  T_ = 5
-  rmsea_stat[i] <- sum(sapply(  0:T_, 
-                             function(k) sum( ( var_ccov(A,Z,Delta=k) - var_ccov(A_,Z_,Delta=k) )^2 )))
+  # Compute a statistic for RMSE(A), up to arbitrary T = 72 = maximum time points observed for a person.
+  T_ = 72
+  rmsea_stat[i] <- sum(sapply(  1:T_, 
+                             function(k) sum( ( var_ccov(A,Z,Delta=k) - var_ccov(A_,Z_,Delta=k) )^2 ))) +
+    sum( ( (var_ccov(A,Z,Delta=0) - var_ccov(A_,Z_,Delta=0))[tril(selector)] )^2 ) #FIX
 }
 
 # Close progress bar
 close(pb)
 
+
+
+if(F) {
+  saveRDS(mad_draws, "mad_draws.RDS")
+  saveRDS(mse_draws, "mse_draws.RDS")
+  saveRDS(rmse_draws, "rmse_draws.RDS")
+  saveRDS(rmsea_stat, "rmsea_stat.RDS")
+  
+  mad_draws <- readRDS("mad_draws.RDS")
+  mse_draws <- readRDS("mse_draws.RDS")
+  rmse_draws <- readRDS("rmse_draws.RDS")
+  rmsea_stat <- readRDS("rmsea_stat.RDS")
+  
+}
+
+
 # Compute RMSEA with 95%CI. This is heuristic, since the covariance in this case is arbitrarily large and
 # as such there is no way to really compute df_...
-df_ <- (nrow(A)*(nrow(A)+1))/2 - (length(varLabs) + 1) # factor loadings count plus CF autoregression.
-sqrt(max(c(mean(rmsea_stat, na.rm = T) - df_, 0)) / (df_*(N_-1)))
-mean(RMSEA, na.rm = T); quantile(RMSEA, c(0.05, 0.95))
+N_ = nrow(Data5b)
+df_ <- length(A) + sum(upper.tri(Z,diag = T)) - (length(varLabs) + 1) # factor loadings count plus CF autoregression.
+RMSEA <- sqrt( max( c( ( rmsea_stat - df_ ) / ( df_*(N_-1) ) , 0), na.rm = T) )
+RMSEA <- sqrt( max( c( ( rmsea_stat  ) / ( df_ ) , 0), na.rm = T) )
 
-psych::describe(cbind(mad_draws, mse_draws, rmse_draws))
+psych::describe(cbind(mad_draws, mse_draws, RMSEA))
 
 # MAD, MSE densities
 ggplot() +
@@ -190,7 +199,7 @@ ggplot() +
 ggplot() +
   geom_density(aes(x = mse_draws))
 ggplot() +
-  geom_density(aes(x = rmsea_stat))
+  geom_density(aes(x = RMSEA))
 
 
 
@@ -303,27 +312,30 @@ if(F) {
 
 # Extract residual covariance matrix for individual or whole dataset
 # Summary of the model to review residual correlations
-summary(res_bayes_all)
+summary(res_bayes_second)
 
 # retrieve A
-A <- matrix(summary(res_bayes_all)$fixed$Estimate[4:12], ncol = 3, nrow = 3)
+estimates <- summary(res_bayes_second)$fixed$Estimate
+estimates[ -c(1:length(varLabs2)) ]
+A <- matrix(estimates[ -c(1:length(varLabs2)) ], 
+            ncol = length(varLabs2), nrow = length(varLabs2))
 
 # retrieve Z
-Z_cor <- summary(res_bayes_all)$rescor_pars$Estimate
-Z <- matrix(0,ncol = 3, nrow = 3)
+Z_cor <- summary(res_bayes_second)$rescor_pars$Estimate
+Z <- matrix(0,ncol = nrow(A), nrow = ncol(A))
 diag(Z) <- 1
 Z[lower.tri(Z, diag = F)] <- Z_cor
 Z[upper.tri(Z, diag = F)] <- Z_cor
 
-Z <- diag(summary(res_bayes_all)$spec_pars$Estimate) %*%
+Z <- diag(summary(res_bayes_second)$spec_pars$Estimate) %*%
   Z %*% 
-  diag(summary(res_bayes_all)$spec_pars$Estimate)
+  diag(summary(res_bayes_second)$spec_pars$Estimate)
 
 
 # Obtain the posterior draws for A matrices
 
-draws <- as_draws_df(res_bayes_all)
-A_draws <- draws %>% select(matches("^b_.*_lag$"))
+draws_second <- as_draws_df(res_bayes_second)
+A_draws <- draws_second %>% select(matches("^b_.*_lag$"))
 A_array <- array(0, dim = c(nrow(A), ncol(A), nrow(A_draws) ))
 for( i in 1:nrow(A_draws)) {
   A_array[ , , i ] <- matrix(as.numeric(A_draws[ i , ], ncol = 3, nrow = 3))
@@ -333,30 +345,30 @@ for( i in 1:nrow(A_draws)) {
 # Convert posterior samples to a data frame
 
 # Extract residual standard deviations and correlations using regex
-residual_sd_draws <- draws %>% select(matches("^sigma"))
-residual_cor_draws <- draws %>% select(matches("^rescor"))
+residual_sd_draws_second <- draws_second %>% select(matches("^sigma"))
+residual_cor_draws_second <- draws_second %>% select(matches("^rescor"))
 
 # Compute posterior draws of the covariance matrix
-Z_array <- array(0, dim = c(nrow(Z), ncol(Z), nrow(residual_sd_draws) ))
-for( i in 1:nrow(residual_sd_draws)) {
+Z_array <- array(0, dim = c(nrow(Z), ncol(Z), nrow(residual_sd_draws_second) ))
+for( i in 1:nrow(residual_sd_draws_second)) {
   
   #Transform to covariance
-  Z_cor_temp <- as.numeric(residual_cor_draws[ i , ])
-  Z_temp <- matrix(0,ncol = 3, nrow = 3)
+  Z_cor_temp <- as.numeric(residual_cor_draws_second[ i , ])
+  Z_temp <- matrix(0,ncol = ncol(A), nrow = nrow(A))
   diag(Z_temp) <- 1
   Z_temp[lower.tri(Z_temp, diag = F)] <- Z_cor_temp
   Z_temp[upper.tri(Z_temp, diag = F)] <- t(Z_temp)[upper.tri(Z_temp)]
   
-  Z_temp <- diag(as.numeric(residual_sd_draws[  i , ])) %*%
+  Z_temp <- diag(as.numeric(residual_sd_draws_second[  i , ])) %*%
     Z_temp %*% 
-    diag(as.numeric(residual_sd_draws[  i , ]))
+    diag(as.numeric(residual_sd_draws_second[  i , ]))
   
   #place.
   Z_array[ ,, i] <- Z_temp
   
 }
 
-# Obtain the closest indistinguishable model, and inference based on the draws from the posterior. -----
+## Inference. -----
 
 # Cross-covariance function
 var_ccov <- function(A,Z,Delta) {
@@ -365,11 +377,11 @@ var_ccov <- function(A,Z,Delta) {
                                      fastmatrix::vec(Z), ncol = ncol(A), nrow = nrow(A)))
 }
 
-pb <- txtProgressBar(min = 0, max = nrow(draws), style = 3)  # Progress bar
-mad_draws <- c()
-mse_draws <- c()
-rmse_draws <- c()
-rmsea_stat <- c()
+pb <- txtProgressBar(min = 0, max = nrow(draws_second), style = 3)  # Progress bar
+mad_draws_second <- c()
+mse_draws_second <- c()
+rmse_draws_second <- c()
+rmsea_stat_second <- c()
 N_ <- nrow(Data5b %>%
              na.omit())
 for( i in seq(1,N_,by = 10)) {
@@ -381,33 +393,51 @@ for( i in seq(1,N_,by = 10)) {
   A_ <- results$A_result
   Z_ <- results$Z_result
   # Compute some summaries of the differences
-  mad_draws[ i ] <- mean(abs(c(c(A_ - A),c(Z_ - Z))))
-  mse_draws[i] <- mean((c(A_ - A)^2 + c(Z_ - Z)^2))
-  rmse_draws[i] <- sqrt(mean((c(A_ - A)^2 + c(Z_ - Z)^2)))
-  # Compute a statistic for RMSE(A), up to arbitrary T = 5.
-  T_ = 5
-  rmsea_stat[i] <- sum(sapply(  0:T_, 
-                                function(k) sum( ( var_ccov(A,Z,Delta=k) - var_ccov(A_,Z_,Delta=k) )^2 )))
+  mad_draws_second[ i ] <- mean(abs(c(c(A_ - A), c(Z_ - Z))))
+  mse_draws_second[i] <- mean((c(A_ - A)^2 + c(Z_ - Z)^2))
+  rmse_draws_second[i] <- sqrt(mean((c(A_ - A)^2 + c(Z_ - Z)^2)))
+  # Compute a statistic for RMSE(A), up to arbitrary T = 72 = maximum of observation time length in data.
+  T_ = 72
+  rmsea_stat_second[i] <- sum(sapply(  1:T_, # This is actually simply the fit, F, statistic (for unweighted least squares).
+                                function(k) sum( ( var_ccov(A,Z,Delta=k) - var_ccov(A_,Z_,Delta=k) )^2 ) )) +
+    sum( (fastmatrix::vech( var_ccov(A,Z,Delta=0) - var_ccov(A_,Z_,Delta=0) ))^2 ) # Only use the non-redundant elements of the within time point covariance.
 }
+
+
+if(F) {
+  saveRDS(mad_draws_second, "mad_draws_second.RDS")
+  saveRDS(mse_draws_second, "mse_draws_second.RDS")
+  saveRDS(rmse_draws_second, "rmse_draws_second.RDS")
+  saveRDS(rmsea_stat_second, "rmsea_stat_second.RDS")
+  
+  mad_draws_second <- readRDS("mad_draws_second.RDS")
+  mse_draws_second <- readRDS("mse_draws_second.RDS")
+  rmse_draws_second <- readRDS("rmse_draws_second.RDS")
+  rmsea_stat_second <- readRDS("rmsea_stat_second.RDS")
+  
+  }
+
 
 # Close progress bar
 close(pb)
 
-# Compute RMSEA with 95%CI. This is heuristic, since the covariance in this case is arbitrarily large and
-# as such there is no way to really compute df_...
-df_ <- (nrow(A)*(nrow(A)+1))/2 - (length(varLabs) + 1) # factor loadings count plus CF autoregression.
-sqrt(max(c(mean(rmsea_stat, na.rm = T) - df_, 0)) / (df_*(N_-1)))
-mean(RMSEA, na.rm = T); quantile(RMSEA, c(0.05, 0.95))
+# Compute RMSEA with 95%CI. 
+df_ <- 49 + 28 - 7 - 1
+df_ <- length(A) + sum(upper.tri(Z, diag = T)) - (length(varLabs2) + 1) # factor loadings count plus CF autoregression.
+RMSEA_second <- sqrt(rmsea_stat_second/df_)
+median(RMSEA_second, na.rm = T)
 
-psych::describe(cbind(mad_draws, mse_draws, rmse_draws))
+psych::describe(cbind(mad_draws_second, 
+                      mse_draws_second, 
+                      RMSEA_second))
 
 # MAD, MSE densities
 ggplot() +
-  geom_density(aes(x = mad_draws))
+  geom_density(aes(x = mad_draws_second))
 ggplot() +
-  geom_density(aes(x = mse_draws))
+  geom_density(aes(x = mse_draws_second))
 ggplot() +
-  geom_density(aes(x = rmsea_stat))
+  geom_density(aes(x = RMSEA_second))
 
 
 
