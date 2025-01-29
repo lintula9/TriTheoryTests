a571c68a1c2c2abbf98f7583e748276877298c08
Sakari Lintula <lintula8@gmail.com>
2025-01-29 10:56:27 +0200

    Adjustmnets to plotting and so forth. Looks good.

--- Supplementary Appendix R -script empiric example.R
+++ Supplementary Appendix R -script empiric example.R
@@ -11,20 +11,16 @@ required_packages <- c(
   "Matrix", "fastmatrix", "expm", "rstan",
   "qgraph", "tidyverse", 
   "ggplot2", "rstantools", "loo", "bayesplot",
-  "shinystan", "cmdstanr"
+  "shinystan", "cmdstanr", "posterior"
 )
 
 # Function to check and install missing packages
-install_if_missing <- function(packages) {
-  for (pkg in packages) {
+for (pkg in required_packages) {
     if (!requireNamespace(pkg, quietly = TRUE)) {
       install.packages(pkg)
     }
     library(pkg, character.only = TRUE)
   }
-}
-
-install_if_missing(required_packages)
 
 # --------------- 2. Estimation -----------------------------
 
@@ -231,36 +227,22 @@ fit_Net <- mod_Net$sample(
   }
 )
 
-# Possible conversion to matrix and saving.
-fit_Net <- as.matrix(fit_Net); gc() # Rewrite, so that RAM is not occupied.
-saveRDS(fit_Net, file = "Datas/BayesianVAR_3_symptoms_28_01.RDS"); gc()
-
-# Diagnostics
-plotnams <- c("A", "Omega", "cutpoints",
-              "time_of_day_intercept")
-for(i in plotnams){
-  dev.new()
-  print(  mcmc_trace(fit_Net$draws(variables = i)))
-  }; gc()
-# Pairwise plots, for multicollinearit:
-pairplotter <- function(var1,var2,fit = fit_Net) {
-  mcmc_pairs(fit_Net, regex_pars = c(var1,var2))
+# Read data
+if(F){
+fit_Net <- as_cmdstan_fit(files = paste0("Datas/fit_net_3var_output-202501290753-",1:8,"-5996ed.csv") ); gc()
+draws_df <- as_draws_df(fit_Net$draws(variables = c(inference_vars_regex, "lp__") )); gc(); rm(fit_Net); gc()
 }
-
-# Extract parameters
-A_mean <- matrix(0, ncol = K, nrow = K)
-for(i in 1:K) for(j in 1:K){
-  A_mean[i,j] <- mean(as.vector(fit_Net[,paste0("A[",i,",",j,"]")]))
-  }
-Omega_mean <- matrix(0, ncol = K, nrow = K)
-for(i in 1:K) for(j in 1:K){
-  Omega_mean[i,j] <- mean(as.vector(fit_Net[,paste0("Omega[",i,",",j,"]")]))
-}
+# Diagnostics and posterior distribution marignal plots. 
+plotnams <- c("A", "Omega", "cutpoints","time_of_day_intercept");pdf(file = paste0("Datas/Bayespots_",format(Sys.time(), "%Y-%m-%d"), ".pdf"))
+for(i in plotnams){print(  mcmc_trace(draws_df[ , grep(i, names(draws_df))]) );print(  mcmc_areas_ridges(draws_df[ , grep(i, names(draws_df))]) );
+  }; gc(); dev.off()
+# Extract maximum a posterior (approximated) VAR parameters:
+map_index <- which.max(draws_df$lp__);A <- matrix( unlist(draws_df[map_index,grep("A", names(draws_df))]), ncol = K, nrow = K);
+Z <- matrix( unlist(draws_df[map_index,grep("Omega", names(draws_df))]), ncol = K, nrow = K)
 
 # Plot the Network:
-par(mfrow=c(1,2))
-qgraph(A_mean);qgraph(Omega_mean)
-par(mfrow=c(1,1))
+par(mfrow=c(1,2)); pdf(file = paste0("Datas/Bayes_MAP_AZestimates",format(Sys.time(), "%Y-%m-%d"), ".pdf"))
+qgraph(input = as.matrix(A));qgraph(input = as.matrix(Z));dev.off();par(mfrow=c(1,1));
 
 
 
@@ -392,6 +374,8 @@ fit_Net_7 <- sampling(stan_model_Net, data = stan_data,
                         time_of_day_intercept = replicate(stan_data$nbeeps, rep(0, stan_data$K), simplify = FALSE)
                       )}); gc()
 
+
+
 # Split the draws to inference set, likelihood set and nuisance set
 fit_Net_7 <- as.matrix(fit_Net_7); gc() # Rewrite, so that RAM is not occupied.
 saveRDS(as.matrix(fit_Net_7)[,dimnames(fit_Net_7)$parameters %in% nuisance_vars], "Datas/BayesOrderedVAR_FIT_7_NUISANCE_POSTERIOR.RDS", compress = T); gc()
@@ -401,7 +385,7 @@ saveRDS(fit_Net_7[, grepl("log_lik_7", dimnames(fit_Net_7)$parameters) ], "Datas
 fit_Net_7 <- fit_Net_7[, dimnames(fit_Net_7)$parameters %in% inference_vars ]; gc()
 saveRDS(fit_Net_7, file ="Datas/BayesOrderedVAR_FIT_7_INFERENCE_POSTERIOR_27_01.RDS"); gc()
 # Diagnostics
-plotnams <- c("A","B", "Omega", "cutpoints",
+plotnams <- c("A", "Omega", "cutpoints",
               "time_of_day_intercept")
 for(i in 1:length(plotnams)){
   dev.new()
@@ -434,33 +418,4 @@ for(i in 1:K) for(j in 1:K){
 par(mfrow=c(1,2))
 qgraph(A_mean);qgraph(Omega_mean)
 par(mfrow=c(1,1))
-
-
-
-
-  ## DCF ------
-# Run the DCF model
-stan_model_DCF <- stan_model(file = "BayesianOrderedDCF.stan")
-fit_dcf <- sampling(stan_model_DCF, data = stan_data, 
-                iter = 2000, chains = 4, cores = 4, 
-                control = list(adapt_delta = 0.80) )
-saveRDS(fit_dcf, "Datas/BayesOrderedDCF_FIT.RDS", compress = T); gc()
-# Diagnostics
-print(fit_dcf, pars = c("psi", "Lambda", "cutpoints", paste0("eta[",1:10,"]"), 
-                        paste0("eta_zero[",1:5,"]"),
-                        "subject_intercept"))
-dev.new(noRStudioGD = T)
-traceplot(fit_dcf)
-dev.new(noRstudioGD = T)
-traceplot(fit_dcf, pars = "cutpoints")
-dev.new(noRstudioGD = T)
-traceplot(fit_dcf, pars = c("time_of_day_intercept", "eta_innovation"))
-check_hmc_diagnostics(fit_dcf)
-
-# WAIC and LOO
-log_lik_matrix_dcf <- extract_log_lik(fit_dcf, parameter_name = "log_lik", merge_chains = FALSE)
-waic_results_dcf <- waic(log_lik_matrix_dcf)
-print(waic_results_dcf)
-loo_results_dcf <- loo(log_lik_matrix_dcf)
-print(loo_results_dcf)
 
