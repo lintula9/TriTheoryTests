6fdca99b80d4c6403896fb2775c41005fc80cae9
lintula9 <123648372+lintula9@users.noreply.github.com>
2025-03-19 10:45:41 +0200

    Coming together. Empiric section will see a revision. Stepwise approach seems good: Start with eigenvalues of cross-covariances and proportion explained. Then continuo onto factor congruency. Then apply RMSEA (if it converges?).

--- Supplementary Appendix R -script civ_find.R
+++ Supplementary Appendix R -script civ_find.R
@@ -1,11 +1,16 @@
 # Readme: 
 
 
-# Run the whole script, then civ_find takes the coefficient matrix,
-# innovation covariance as arguments. If you want to compute RMSEA, you also
-# need to give sample size N and specify cov.difference = T (default). In this case, we need
-# diagonal errors to be allowed - otherwise the maximum likelihood criterion is
-# not solvable (because the implied covariance will be (in practice nearly) low rank).
+# First, run (source) the whole script. 
+# 1) civ_find takes the coefficient matrix and innovation covariance as arguments. 
+# If you want to compute RMSEA, you also
+# need to give sample size N and specify cov.difference = T (default). 
+# 2) civ_parallel takes the coefficient matrix and innovation covariance as arguments.
+# You can define time_points used in the computation. 
+
+#####################################################
+# See numerical examples at the end of this script! #
+#####################################################
 
 
 # Helper functions -------------
@@ -29,11 +34,11 @@ var_ccov <- function(coefmat,innocov,Delta) {
                    ncol = ncol(coefmat), nrow = nrow(coefmat)))
 }
 
-# Helper function to form M times M block matrix, of the (cross-)covariances.
+# Helper functions to form M times M block matrices, of the (cross-)covariances. I.e., TK times TK covariance matrices.
 var_ccov_Mblock <- function(A, Z, M) {
   K <- nrow(A)      # dimension of each block (A is K x K)
-  big_dim <- M * K  # dimension of final matrix
-  S_big <- matrix(0, nrow = big_dim, ncol = big_dim)
+  # Placeholder.
+  S_big <- matrix(0, nrow = M * K, ncol = M * K)
   
   for (i in seq_len(M)) {
     for (j in seq_len(M)) {
@@ -56,7 +61,32 @@ var_ccov_Mblock <- function(A, Z, M) {
   return(S_big)
 }
 
-                # Main function --------------
+dcf_ccov_Mblock <- function(L, psi, M) {
+  K       <- length(L) 
+  # Placeholder.
+  S_big <- matrix(0, nrow = M * K, ncol = M * K )
+  
+  for (i in 1:M) {
+    for (j in 1:M) {
+      
+      delta <- abs(i - j)
+      block_ij <- if(delta == 0) tcrossprod(L) else psi^delta * tcrossprod(L)
+      
+      if (i > j) {
+        block_ij <- t(block_ij)
+      }
+      
+      row_idx <- ((i-1)*K + 1) : (i*K)
+      col_idx <- ((j-1)*K + 1) : (j*K)
+      
+      S_big[row_idx, col_idx] <- block_ij
+    }
+  }
+  
+  return(S_big)
+}
+
+                # Main functions --------------
 
 # Closest VAR(1) model - indistinguishable from a dynamic common factor model. 
 
@@ -71,18 +101,22 @@ civ_find <- function(A, Z,
                      N = NULL,
                      error_ratio = 0.5) {
   
-  {if(time_points < 2) simpleError("time_points must be 2 or larger.")
-
+  if(time_points < 2) simpleError("time_points must be 2 or larger.")
   K <- ncol(A)
+  
+  {
+
+    # Initial values for optimizers.
   if(!random.init){
-    # Initial values are taken based on A.
     Lambda <- Re(eigen(A)$vectors[,1])
     psi    <- Re(eigen(A)$values[1])} else {
-    Lambda <- rnorm(K,sd=sd(vec(A)))
-    psi    <- min(rexp(1), 0.9)    }
+    Lambda <- rnorm(K,0.5)
+    psi    <- runif(1, 0.5, 0.95)    }
   
-  omega0 <- rexp(K) 
-
+  omega0   <- rexp(K, rate = 5)
+  
+  }
+  
   if(!cov.difference){
     
   message("Minimizing distance in terms of parameters (not covariance), as cov.difference was set to False.")
@@ -134,7 +168,7 @@ civ_find <- function(A, Z,
     ))}
   
   
-  }
+  
   
   if(cov.difference) {
     # Population covariance.
@@ -156,11 +190,11 @@ civ_find <- function(A, Z,
       Lambda <- theta[2:(K+1)]             # length K
       omega0 <- theta[(K+2):(2*K+1)]       # length K
       
-      A_indistinguishable = psi * diag(1,ncol(A),nrow(A))        # A_indistinguishable
+      A_indistinguishable = psi * tcrossprod(Lambda) / sum(Lambda^2) # A_indistinguishable
       Z_indistinguishable = (1-psi^2) * tcrossprod(Lambda)
 
       # 2) Compute the implied covariance.
-      S_implied <- var_ccov_Mblock(A_indistinguishable, Z_indistinguishable, time_points) + diag(rep(omega0, times = time_points), time_points*K, time_points*K)
+      S_implied <- dcf_ccov_Mblock(Lambda, psi, time_points) + diag(rep(omega0, times = time_points), time_points*K, time_points*K)
       
       if(return_cov) {return(S_implied)}
       
@@ -170,12 +204,11 @@ civ_find <- function(A, Z,
       return(F_ML = log(det(S_implied)) + 
                     sum(diag( solve(S_implied) %*% S )) - 
                     log(det(S)) - 
-                    2*K) 
-    # Note: 2*K, because we have 2K variables.
+                    time_points*K) 
     }
     
     # Maximum Likelihood statistic, with constraints:
-    # Error variances and serial covariances positive.
+    # Error variances (perturbations in the Supplementary Material) and serial covariances positive.
     # The CF is implicitly assumed to have unit variance.
     # DCF is constrained stationary.
     lower_bounds  <- c(-1, rep(-Inf, times = K), rep(0,   times = K))
@@ -190,21 +223,20 @@ civ_find <- function(A, Z,
       upper  = upper_bounds,
       control = list(maxit = n.iter))
     
-    # Return T statistic, which is distributed ~ Chisq_DF(F_ML - DF)
+    # Return T statistic.
     statistic = (N-1) * F_ML$value
 
-    # Compute degrees of freedom
-    DF    = K*K + length(fastmatrix::vech(Z)) - (K + 1)
+    # Compute degrees of freedom.
+    DF    = time_points*K*(time_points*K + 1)/2 - (K + 1)
     
-    # Compute RMSEA
+    # Compute RMSEA.
     RMSEA = sqrt( max(0, statistic - DF) / (DF*(N-1)) )
     
-    # Return implied covariance
+    # Return implied covariance.
     S_implied <- F_criterion(theta = F_ML$par, S = S, K = K, return_cov = T)
     
     # Warnings.
-    if(F_ML$value < 0) warning("Negative discrepancy observed. Likely the implied covariance was non-invertible.
-                               \n Results are not interpretable")
+    if(F_ML$value < 0) warning("Negative discrepancy observed.\nResults are not interpretable")
     
     # Return results
     return(list(
@@ -229,12 +261,11 @@ RMSEA_approx <- function(A, Z, N = NULL, error_ratios = seq(0.01, 1, length.out 
   rmsea_samples <- as.vector(pbapply::pbsapply(error_ratios, 
                                                    FUN = function(x) {
                                                      rmsea <- try(civ_find(A, Z, error_ratio = x, 
-                                                                           N = N, time_points = 2,
-                                                                           cov.difference = T, ...)$RMSEA,
+                                                                           N = N, cov.difference = T, ...)$RMSEA,
                                                                   silent = T)
                                                      return(if(is.character(rmsea)) NA else rmsea)}))
   
-  # Fit a quadratic regression model
+  # Fit a polynomial regression model
   fit <- try(lm(rmsea_samples ~ error_ratios + I(error_ratios^2) + I(error_ratios^3)), silent = TRUE)
   
   pred_error_ratios <- seq(0, max(error_ratios), length.out = 30)
@@ -252,7 +283,7 @@ RMSEA_approx <- function(A, Z, N = NULL, error_ratios = seq(0.01, 1, length.out 
   
   return(result)
   }
-
+  # Plot method for the RMSEA approximation.
 plot.rmsea_approximation <- function(x, ...) {
   if (!inherits(x, "rmsea_approximation")) {
     stop("Input must be of class 'rmsea_approximation'.")
@@ -273,6 +304,9 @@ plot.rmsea_approximation <- function(x, ...) {
   
   # Add quadratic regression line
   lines(pred_error_ratios, predicted, lty = 2)
+  
+  # Add a cross mark to what RMSEA we predict.
+  points(x = 0, y = predicted[1], pch = "x", cex = 1.5)
   
   # Add RMSEA approximation text
   text(0.2, max(max(rmsea_samples, na.rm = TRUE), 0.25) - 0.01, 
@@ -314,6 +348,7 @@ civ_parallel <- function(A,Z,time_points = 10) {
 if(F){
   
   # When the fit is bad:
+    # Create a VAR(1) model.
   A_2 <- matrix(c(
     0.5, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0,
     0.0, 0.4, 0.1, 0.0, 0.0, 0.0, 0.0,
@@ -323,25 +358,50 @@ if(F){
     0.0, 0.0, 0.0, 0.0, 0.0, 0.4, 0.1,
     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2
   ), nrow = 7, byrow = TRUE)
-  
-  # Identity on the diagonal; 0.2 for first off-diagonals
   Z_2 <- diag(7)
   Z_2[cbind(1:6, 2:7)] <- 0.2
   Z_2[cbind(2:7, 1:6)] <- 0.2
   
-  # Predicted covariance
+    # Predicted covariance, up to one time point increment.
   var_ccov_Mblock(A_2, Z_2, M = 2)
   
-  # The fit is bad.
-  result  <- civ_find(A_2, Z_2, N = 4200, cov.difference = T, random.init = T, time_points = 2, 
-                     error_ratio = 0.5)
+    # The fit is bad.
+  rmsea_approximations <-   RMSEA_approx(A = A_2, Z = Z_2, N = 4200) 
+  plot(rmsea_approximations) # Plot, if we want to assess if the RMSEA approximation worked nicely.
+
+    # There are no large jumps in eigenvalues of the cross-covariances: No eigenvalue is dominant...
+  matplot(sapply(0:5, function(t) eigen(var_ccov(A_2,Z_2,t))$values), type =  "b", ylab = "Eigenvalues"); grid()
+    # ... and, consequently, proportion explained index is not high. 
+  civ_parallel(A_2, Z_2)$prop_explained[1]
+  plot(civ_parallel(A_2, Z_2)$prop_explained, ylab = "proportion explained"); grid()
+    # Also factor loading congruency (for the first factor) drops quickly, suggesting non-invariant loadings (if a factor was present).
+  plot( civ_parallel(A_2, Z_2)$all_factor_congruencies[,1] ); grid()
   
-  # Better approximation to RMSEA:
-  rmsea_approximations <-   RMSEA_approx(A = A_2, Z = Z_2, N = 4200)
   
-  plot(rmsea_approximations)
+  # When the fit is good:
+    # Create a VAR(1) model.
+  set.seed(123)
+  lambdas <- tcrossprod(rep(0.4, times = 7) + rnorm(7, sd = 0.1 ))
+  A_3 <- (0.5 *     lambdas ) + matrix(rnorm(7*7, sd = 0.1), ncol = 7) 
+  Z_3 <- tcrossprod(lambdas)  + matrix(rnorm(7*7, sd = 0.1), ncol = 7) 
   
-  # Proportion explained is not high, and factor congruency (for the first factor) drops quickly suggesting non-invariant loadings.
-  civ_parallel(A_2, Z_2)
+    # Predicted covariance, up to one time point increment.
+  var_ccov_Mblock(A_3, Z_3, M = 2)
   
-}
+    # There is a significant jump in eigenvalues of the cross-covariances: The first one is dominant.
+  matplot(sapply(0:5, function(t) eigen(var_ccov(A_3,Z_3,t))$values), type =  "b", ylab = "Eigenvalues"); grid()
+  
+    # Consequently, the proportion explained index is near perfect (1). (A warning might occur, noting that eigenvalues become imaginary.)
+  civ_parallel(A_3,Z_3)$prop_explained[1]
+  plot(
+    # To approximate, we only use real parts of the eigenvalues with 'Re'.
+    Re(civ_parallel(A_3,Z_3)$prop_explained), ylab = "proportion explained"); grid()
+  
+    # Also factor loading congruency is stable at > 0.95, suggesting invariant loadings (if a factor was present).
+  plot( civ_parallel(A_3,Z_3)$all_factor_congruencies[,1] , ylim = c(0.5,1)); grid()
+  
+    # The RMSEA cannot be computed, since 
+  rmsea_approximations_2 <- RMSEA_approx(A = A_3, Z = Z_3, N = 4200) 
+
+
+  }
