a08b8de04ac39b030f905dd9ea9a27dfab1d6de0
Sakari Lintula <lintula8@gmail.com>
2025-05-07 22:10:38 +0300

    Old changes shouldve been commited before.

--- Supplementary Appendix R -script numerical examples.R
+++ Supplementary Appendix R -script numerical examples.R
@@ -38,8 +38,15 @@ for( i in 1:4){
   coefs <- coef(estimated_var)
   As[[i]] <- do.call(cbind, lapply(coefs, FUN = function(x) x[1:4,"Estimate"]))
 }
-tiff(filename = "Figure_1.tiff", width = 8, height = 6, units = "in", res = 480)
+tiff(filename = "Figure_1.tiff", 
+     width    = 17, 
+     height   = 19, 
+     units    = "cm", 
+     res      = 300,
+     pointsize = 10)
+
 par(mfrow = c(2,2))
+par(mar   = c(4,4,2,0.5))
 qgraph(As[[1]], layout = "circle")
 qgraph(As[[2]], layout = "circle")
 qgraph(As[[3]], layout = "circle")
