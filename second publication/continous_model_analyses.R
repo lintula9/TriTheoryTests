1e2a5a763c87dbc3b912e76b8609adb24db214bd
Sakari Lintula <lintula8@gmail.com>
2026-03-11 21:48:30 +0200

    Parameter estimates are wrong.

--- second publication/continous_model_analyses.R
+++ second publication/continous_model_analyses.R
@@ -3,37 +3,43 @@ source("./second publication/continuous_decompose.R")
 # http://journals.sagepub.com/doi/suppl/10.1177/21677026241301057
 # The most complex models are used - as they were best fitting.
 
+# Ask for parameter estimates from Jaakko.
+
 # Clinical cohort model: Depression iCBT.
 Tammilehto_2025_depiCBT_A <- matrix(
-  c(-.3191,  .0343  , 
-     .0498, -.2334 ), 2, 2, byrow = TRUE)
+  c(-.3250,  .0558  , 
+     .0431, -.2442 ), 2, 2, byrow = TRUE)
 Tammilehto_2025_depiCBT_Q <- matrix(
-  c(.2219, .1876, 
-    .1876, .2666), 2, 2, byrow = TRUE)
+  c( .2244,  .1881, 
+     .1881,  .2664), 2, 2, byrow = TRUE)
 continuous_decompose(Tammilehto_2025_depiCBT_A,
                      Tammilehto_2025_depiCBT_Q)
-# BELOW NOT DONE.
+
 # Clinical cohort models: GAD iCBT.
-Tammilehto_model_8_A <- matrix(
+Tammilehto_2025_gadiCBT_A <- matrix(
   c(-.191,  .034  , 
      .044, -.137 ), 2, 2, byrow = TRUE)
-Tammilehto_model_8_Q <- matrix(
+Tammilehto_2025_gadiCBT_Q <- matrix(
   c(.233^2,         .233*.292*.467, 
     .233*.292*.467, .292^2), 2, 2, byrow = TRUE)
 
 # Clinical cohort models: Pooled iCBT.
-Tammilehto_model_12_A <- matrix(
-  c(-.297,  .041, 
-    -.016, -.164 ), 2, 2, byrow = TRUE)
-Tammilehto_model_12_Q <- matrix(
-  c(.304^2,         .304*.318*.504, 
-    .304*.318*.504, .318^2), 2, 2, byrow = TRUE)
+Tammilehto_2025_pooliCBT_A <- matrix(
+  c(-4.2833,  .4641, 
+      .1359, -3.4495 ), 2, 2, byrow = TRUE)
+Tammilehto_2025_pooliCBT_Q <- matrix(
+  c(.2953, .0619,
+    .0619, .1612), 2, 2, byrow = TRUE)
 
-# Combine Tammilehto et al., 2025 esimates. ----
+## Combine Tammilehto et al., 2025 estimates. ----
 tammilehto_2025 <- lapply(
-  paste0("Tammilehto_model_", 1:12, "_"),
+  grep("Tammilehto_2025_(.*)_*", ls(), value = T,) |> 
+    gsub(pattern = "(_A|_Q)", replacement = "_") |> 
+    unique(),
   FUN = \(model_name) {
     A <- get(paste0(model_name, "A"))
     Q <- get(paste0(model_name, "Q"))
     return(continuous_decompose(A,Q))
       })
+
+# Tammilehto et al., 2026. ----
