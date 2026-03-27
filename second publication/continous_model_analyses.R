cce331dd3d2edca9a2b48ffc99aa377eb7883be7
lintula9 <123648372+lintula9@users.noreply.github.com>
2026-03-27 16:23:25 +0200

    Add the 2026 pre-print models.

--- second publication/continous_model_analyses.R
+++ second publication/continous_model_analyses.R
@@ -68,3 +68,9 @@ Fried_2022_detrended_Q <- read.csv("./Datas/fried_2022_Q.csv") |>
 decompose_stationary_cov(Fried_2022_detrended_A,
                          Fried_2022_detrended_Q,
                          type = "discrete")
+# Lintula et al., 2026. ----
+A_7 <- readRDS("C:/Users/lintu/Documents/TriTheoryTests/Datas/A_7.RDS")
+Z_7 <- readRDS("C:/Users/lintu/Documents/TriTheoryTests/Datas/Z_7.RDS")
+decompose_stationary_cov(A_7,
+                         Z_7,
+                         type = "discrete")
